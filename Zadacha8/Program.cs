a4d9a4c43d741ec2a49942a85d6a7c65bb39b7bd
Elnaza <helzahggl@gmail.com>
2023-09-08 14:19:51 +0300

    Закоммичено Zadacha8

---
+++ Zadacha8/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите  число");
+string? first= Console.ReadLine();
+
+Int32.TryParse(first,out int N);
+
+List<int> arr = new List<int>();
+
+for(int i=1; i <N; i++)
+{
+    if(i % 2 == 0)
+    {
+        arr.Add(i);
+    }
+}
+foreach(var s in arr)
+{
+    Console.Write(s + " ");
+}
