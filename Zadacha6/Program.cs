c5548458531eb88c36be3bcdb13c25e55f57e518
Elnaza <helzahggl@gmail.com>
2023-09-08 14:19:38 +0300

    Закоммичено Zadacha6

---
+++ Zadacha6/Program.cs
@@ -0,0 +1,8 @@
+﻿
+Console.WriteLine("Введите число");
+string? first= Console.ReadLine();
+
+Int32.TryParse(first,out int number1);
+
+string res = number1 % 2 == 0 ?  "четное" : "нечетное";
+Console.WriteLine($"Число {number1}  {res}");
